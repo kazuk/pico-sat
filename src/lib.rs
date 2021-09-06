ebec6d6a4d34bc67cfb61785199a85f61ab12341
Kazuhiko Kikuchi <kazuk.dll@kazuk.jp>
2021-09-06 12:28:45 +0900

    split surface api / internal impl

--- src/lib.rs
+++ src/lib.rs
@@ -40,4 +40,55 @@
 mod boolean_expression;
 mod solver;
 
-pub use solver::{solve_all, solve_one, Variables};
+pub use solver::{Literal, Variable, Variables};
+
+/// SAT solve and returns one result
+///
+/// # Arguments
+///
+/// * `input` - vector of vector of literal, outer vector is AND set, inner vector is OR set.
+///
+/// # Return Value
+///
+/// * `Option::Some(Vec<Literal>)` SAT answer
+/// * `Option::None` UNSAT answer
+///
+/// # Examples
+///
+/// ```rust
+/// use pico_sat::{Variables, solve_one};
+/// let mut vars = Variables::new();
+/// let o = vars.create();
+/// let p = vars.create();
+/// let q = vars.create();
+/// let r = vars.create();
+/// let mut f = vec![
+///    vec![ p.t(), q.t(), r.f() ], //    ( P ||  Q || !R )
+///    vec![ p.f(), r.f() ],        // && (!P || !R )
+///    vec![ r.t() ]                // && ( R)
+/// ];
+/// match solve_one(&mut f) {
+///     Some(answer) => {
+///         // SAT
+///         assert_eq!(answer.len(), 3);
+///         assert_eq!(answer[0], r.t(), "first R");
+///         assert_eq!(answer[1], p.f(), "second ~P");
+///         assert_eq!(answer[2], q.t(), "third Q");
+///         assert!( o.is_dontcare(&answer) );
+///         assert!( p.is_false(&answer) );
+///         assert!( q.is_true(&answer) );
+///         assert!( r.is_true(&answer) );
+///     }
+///     None => {
+///         panic!("UNSAT")
+///     }
+/// }
+/// ```
+pub fn solve_one(input: &mut Vec<Vec<Literal>>) -> Option<Vec<Literal>> {
+    solver::solve_one(input)
+}
+
+/// SAT solve and returns all result
+pub fn solve_all(input: &mut Vec<Vec<Literal>>) -> Vec<Vec<Literal>> {
+    solver::solve_all(input)
+}
