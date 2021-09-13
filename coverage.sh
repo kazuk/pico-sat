e31ae47983a380264bf919f4382dacd235f17129
Kazuhiko Kikuchi <kazuk.dll@kazuk.jp>
2021-09-13 13:54:18 +0900

    update coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 set -eux
-
-rm -rf target/debug/deps/pico_sat-*
-
-export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off"
+export CARGO_BUILD_TARGET_DIR="./target/cover"
+export RUSTFLAGS="-Zinstrument-coverage -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off"
+export RUSTDOCFLAGS="-Cpanic=abort"
 
 cargo +nightly build --verbose
+
+rm -rf ./target/cover/debug/deps/profraw
+mkdir ./target/cover/debug/deps/profraw/
+
+export LLVM_PROFILE_FILE="./target/cover/debug/deps/profraw/coverage-%p-%m.profraw"
+
 cargo +nightly test --verbose
 
-grcov ./target/debug/deps -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" -o lcov.info
-genhtml -o ./target/report/ --show-details --highlight --ignore-errors source --legend lcov.info
+RUSTUP_TOOLCHAIN="nightly" grcov ./target/cover/debug/deps/profraw/ -b ./target/cover/debug/deps -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" -o ./target/lcov.info
